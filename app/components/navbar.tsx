b360b2f0e248bbf0fc38ce39f12f0fee8adf5fd6
brunoguima7 <brunodecamargoguimaraes@gmail.com>
2024-02-15 03:32:23 -0300

    melhorias na nav mobile

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
   return (
     <>
       <div className={`${isMenuOpen ? 'w-full h-full bg-black fixed z-20 opacity-60 top-0 md:hidden' : 'hidden'}`} onClick={changeMenu}></div>
-      <header className={`${isMenuOpen ? 'flex-col h-48 md:h-[4.3rem] md:flex-row' : 'h-[4.3rem] flex-row '} text-md py-2 flex px-5 justify-between fixed w-full top-0 z-20 drop-shadow-md`}>
+      <header className={`${isMenuOpen ? 'flex-col h-60 md:h-[4.3rem] md:flex-row' : 'h-[4.3rem] flex-row '} text-md py-2 flex px-5 justify-between fixed w-full top-0 z-20 drop-shadow-md`}>
         <div className="flex">
           <Link href="/" onClick={()=> setIsMenuOpen(false)}>
             <Image src="/favicon.ico" alt="Logotipo com as iniciais de Bruno Guimarães." width="50" height="50" unoptimized />
@@ -35,8 +35,9 @@ export default function Navbar() {
         </nav>
         {isMenuOpen == true && (
           <div className="flex">
-            <nav className="md:hidden flex flex-col text-lg mb-10 space-y-4 fixed right-4 top-20 text-right">
+            <nav className="md:hidden flex flex-col text-lg mb-10 space-y-5 fixed right-4 top-20 text-right">
               <Link href="/#about" className="text-blue-200" onClick={changeMenu}>Sobre mim</Link>
+              <Link href="/#portfolio" className="text-blue-200">Projetos</Link>
               <Link href="/contact" className="text-blue-200" onClick={changeMenu}>Contato</Link>
             </nav>
           </div>
