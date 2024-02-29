dbf871877532d6e812b9e40a9fb7889e8ef9c749
brunoguima7 <brunodecamargoguimaraes@gmail.com>
2024-02-29 02:21:44 -0300

    alteracao para projetos via api

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link"
 import Image from "next/image"
 
-export default function Projects({ projects }: ProjectsProps) {
+interface Image {
+    url: string,
+    alt: string,
+}
+
+interface Project {
+    link: string,
+    name: string,
+    image: Image,
+}
+
+export default async function Projects() {
+
+    const res = await fetch("https://gist.githubusercontent.com/brunoguima7/d82c83f155e8757f8591819e4d028b24/raw/9c6a19122565d452d586b1fac641a4e9040e1b5f/projects.json")
+
+    const project: Project[] = await res.json()
 
     return (
         <>
             <div className="flex flex-col space-y-14 mt-24 md:mt-32 md:space-y-24 md:ml-20 xl:ml-36">
                 <h2 className="text-center md:text-start text-3xl xl:text-5xl">Projetos Recentes</h2>
                 <ul className="flex flex-col flex-wrap md:flex-row gap-x-20 justify-center md:justify-start space-y-12 md:space-y-0">
-                    {projects.map(({ slug, name, image }, index) => (
-                        <Link href={`/projects/${slug}`} key={name + index}>
+                    {project.map(({ link, name, image }, index) => (
+                        <Link href={link} key={name + index}>
                             <li className="space-y-2">
                                 <div className="flex justify-center">
                                     <Image
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,13 @@
-"use client";
-
 import Projects from "./components/projects"
 import About from "./components/about"
 
 export default function Page() {
-    const projects = [
-        {
-            slug: "calendario",
-            name: "Calendário",
-            image: {
-                url: "https://i.pinimg.com/736x/31/a7/40/31a740fa4dac88750dd699a2bb807e89.jpg",
-                alt: "Calendário"
-          }
-        },
-        {
-            slug: "sv-enterprise-service",
-            name: "SVZ Enterprise",
-            image: {
-                url: "https://i.pinimg.com/736x/d1/45/0c/d1450cfccfc6d9be2f5c8ad33eb0e11f.jpg",
-                alt: "SVZ"
-            }
-        },
-    ]
 
     return (
         <>
             <div>
                 <About />
-                <Projects projects={projects} />
+                <Projects/>
             </div>
         </>
     )
